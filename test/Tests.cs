8ecd35d7ebf37a4cd0ad9327819c468d067c04ae
Mark Broadhurst <Mark.Broadhurst@gmail.com>
2017-03-01 10:44:24 +0000

    Fixed tests

--- test/Tests.cs
+++ test/Tests.cs
@@ -1,45 +1,46 @@
-﻿//using Xunit;
-//using OmdbRest;
-//
-//namespace Tests
-//{
-//    public class Tests
-//    {
-//        [Fact]
-//        public void GetById()
-//        {
-//            var x = Client.GetById("tt1490017");
-//            Assert.Equal(x.Title,"The LEGO Movie");
-//        }
-//
-//        [Fact]
-//        public void GetByTitle()
-//        {
-//            var x = Client.GetByTitle("Star Wars");
-//            Assert.Equal(x.Title,"Star Wars: Episode IV - A New Hope");
-//        }
-//
-//        [Fact]
-//        public void GetByTitleAndYear()
-//        {
-//            var x = Client.GetByTitleAndYear("Matrix",1999);
-//            Assert.Equal(x.Title,"The Matrix");
-//        }
-//
-//        [Fact]
-//        public void SearchByText()
-//        {
-//            var x = Client.SearchByText("Fast and Furious");
-//            var s = x.Search[1];
-//            Assert.Equal(s.Title, "The Fast and the Furious: Tokyo Drift");
-//        }
-//
-//        [Fact]
-//        public void SearchByTextAndYear()
-//        {
-//            var x = Client.SearchByTextAndYear("Godfather", 1974);
-//            var s = x.Search[0];
-//            Assert.Equal(s.Title, "The Godfather: Part II");
-//        }
-//    }
-//}
+﻿using System.Linq;
+using Xunit;
+using OmdbRest;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void GetById()
+        {
+            var x = Client.GetById("tt1490017");
+            Assert.Equal("The LEGO Movie", x.Title);
+        }
+
+        [Fact]
+        public void GetByTitle()
+        {
+            var x = Client.GetByTitle("Star Wars");
+            Assert.Equal("Star Wars: Episode IV - A New Hope", x.Title);
+        }
+
+        [Fact]
+        public void GetByTitleAndYear()
+        {
+            var x = Client.GetByTitleAndYear("Matrix",1999);
+            Assert.Equal("The Matrix", x.Title);
+        }
+
+        [Fact]
+        public void SearchByText()
+        {
+            var x = Client.SearchByText("Fast and Furious");
+            var s = x.Search.First();
+            Assert.Equal("The Fast and the Furious", s.Title);
+        }
+
+        [Fact]
+        public void SearchByTextAndYear()
+        {
+            var x = Client.SearchByTextAndYear("Godfather", 1974);
+            var s = x.Search.First();
+            Assert.Equal("The Godfather: Part II", s.Title);
+        }
+    }
+}
